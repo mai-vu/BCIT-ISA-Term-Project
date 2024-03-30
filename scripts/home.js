ad490cb5b7fb4fba15bee319dc03f01288832f7e
shye <113232835+justshye@users.noreply.github.com>
2024-03-30 16:38:08 -0700

    Remove hard-coded strings

--- scripts/home.js
+++ scripts/home.js
@@ -1,18 +1,18 @@
 // Import the messages object from lang/en/strings.js
 import { messages } from '../lang/en/strings.js';
 
-// const apiUrl = "http://localhost:3000/predict";
 const apiUrl = "https://www.alexkong.xyz/proj/predict";
 
 // Function to replace element contents with strings from messages object
 function replaceElementContents() {
     // Replace title
-    document.getElementById('title').textContent = messages.title;
+    document.getElementById('title').textContent = messages.home;
     // document.getElementById('title').textContent = messages.title;
     // document.getElementById('searchHeader').textContent = messages.searchHeader;
     // document.getElementById('insertRows').textContent = messages.insertRows;
     // document.getElementById('submitQuery').textContent = messages.submitQuery;
     document.getElementById('usageCount').innerText = messages.usageCount
+    document.getElementById('logoutButton').textContent = messages.logout;
 }
 
 // Call the function when the DOM content is loaded
