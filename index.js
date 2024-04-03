57d2c208531cfed22cc01459be3ad51c7225dc7e
mai-vu <vhmai3007@gmail.com>
2024-04-03 16:06:56 -0700

    deleted console.log

--- index.js
+++ index.js
@@ -225,8 +225,6 @@ app.post('/signup', async (req, res) => {
         const newUser = await usersCollection.findOne({ email });
         req.session.uid = newUser._id.toString();
 
-        console.log('User created:', email);
-
         // Generate a JWT token for the user
         const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: '1h' });
 
