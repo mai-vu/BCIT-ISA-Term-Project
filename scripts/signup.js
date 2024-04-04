589c487f806e2b5ef5d30cb0d5852f0b13f8e7e0
mai-vu <vhmai3007@gmail.com>
2024-04-03 21:21:49 -0700

    added login button

--- scripts/signup.js
+++ scripts/signup.js
@@ -3,3 +3,5 @@ import { messages } from '../lang/en/strings.js';
 document.getElementById("emailLabel").textContent = messages.email;
 document.getElementById("passwordLabel").textContent = messages.password;
 document.getElementById("signupButton").textContent = messages.signup;
+document.getElementById("loginButton").textContent = messages.login;
+document.getElementById("goLogIn").textContent = messages.goLogIn;
